0ceb969d8bb022567ab0595065141026b63e918f
Jeel Kalathiya <jeelkalathiya01@gmail.com>
2024-07-01 06:37:35 +0530

    Enhanced Functionality

--- Timer/script.js
+++ Timer/script.js
@@ -1,7 +1,53 @@
+let text = document.getElementsByClassName("text");
 let a = document.getElementById("subButton");
+let b = document.getElementById("resetButton");
+let c = document.getElementById("markButton");
+b.disabled = true;
+c.disabled = true;
+
+let ListTime = document.getElementById("list-time");
+let SubListTime = document.createElement("div");
+ListTime.append(SubListTime);
+
+var x = 0; 
+
+function mark(){
+    let hour = text[0].innerHTML;
+    let minute = text[1].innerHTML;
+    let second = text[2].innerHTML;
+    console.log(hour,minute,second);
+
+    let y = SubListTime;
+
+    let NewDiv = document.createElement("div");
+
+    NewDiv.className = 'Mark-list';
+    y.appendChild(NewDiv);
+
+
+    let NewSubDiv1 = document.createElement("div");
+    NewDiv.appendChild(NewSubDiv1);
+    NewSubDiv1.innerHTML = hour;
+    NewSubDiv1.className = 'Mark-list-child';
+    
+    
+    let NewSubDiv2 = document.createElement("div");
+    NewDiv.appendChild(NewSubDiv2);
+    NewSubDiv2.innerHTML = minute;
+    NewSubDiv2.className = 'Mark-list-child';
+    
+
+    let NewSubDiv3 = document.createElement("div");
+    NewDiv.appendChild(NewSubDiv3);
+    NewSubDiv3.innerHTML = second;
+    NewSubDiv3.className = 'Mark-list-child';
+    
+}
 
 function clicked(){
     a.disabled = true;
+    b.disabled = false;
+    c.disabled = false;
     let p = document.getElementById("inputSecond").value;
     let q = document.getElementById("inputMinute").value;
     let r = document.getElementById("inputHour").value;
@@ -18,13 +64,13 @@ function clicked(){
     r = parseInt(r);
     let SetCount = p + ( q + r * 60 ) * 60;
     let count = SetCount;
-    
-    let text = document.getElementsByClassName("text");
 
     let Decrement = () => {
         if(count < 0){
             clearInterval(x);
             a.disabled = false;
+            b.disabled = true;
+            c.disabled = true;
             return;
         }
         let second = count % 60;
@@ -38,7 +84,25 @@ function clicked(){
 
         count--;
     }
-    var x = setInterval(Decrement,1000);
+    x = setInterval(Decrement,1000);
+}
+
+function resetTimer()
+{
+    clearInterval(x);
+    SubListTime.remove();
+    a.disabled = false;
+    b.disabled = true;
+    c.disabled = true;
+
+    text[0].innerHTML = 0;
+    text[1].innerHTML = 0;
+    text[2].innerHTML = 0;
+    document.getElementById("inputSecond").value = null ;
+    document.getElementById("inputMinute").value = null ;
+    document.getElementById("inputHour").value = null ;
 }
 
 a.addEventListener("click",clicked);
+b.addEventListener("click",resetTimer);
+c.addEventListener("click",mark);
